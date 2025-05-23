b7391787fd2bb79a3260858917abbb0d70f6c2c4
Chintan Patel <chintanpatelit024@gmail.com>
2025-05-23 12:21:38 +0530

    Added: Implementing-MVC-in-Tank

---
+++ Assets/Scripts/TankView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TankView : MonoBehaviour
+{
+    private TankController tankController;
+
+    public TankView() {}
+
+    public void SetTankController(TankController _tankController)
+    {
+        tankController = _tankController;
+    }
+}
